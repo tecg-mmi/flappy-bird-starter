85c6c90ef643d33964928df9a109b80dd5a69729
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2026-03-25 11:50:13 +0100

    Initial commit

---
+++ src/ts/Background.ts
@@ -0,0 +1,30 @@
+import {IFrame} from "./framework26/interfaces/IFrame";
+import {IBackground} from "./IBackground";
+
+export class Background implements IBackground {
+
+    sprite: HTMLImageElement;
+    ctx: CanvasRenderingContext2D;
+    frame: IFrame;
+
+
+    constructor(background: IBackground) {
+        this.sprite = background.sprite;
+        this.frame = background.frame;
+        this.ctx = background.ctx;
+    }
+
+    draw() {
+        this.ctx.drawImage(this.sprite,
+            this.frame.sx,
+            this.frame.sy,
+            this.frame.sw,
+            this.frame.sh,
+            this.frame.dx,
+            this.frame.dy,
+            this.frame.dw,
+            this.frame.dh,
+        )
+    }
+
+}
