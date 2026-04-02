4b3d58bba8dbffd38dec7bc80f8361300ed6890c
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2026-04-02 20:17:23 +0200

    Add Main and Background classes with animation setup

---
+++ src/ts/IBackground.ts
@@ -0,0 +1,7 @@
+import {IFrame} from "./framework26/interfaces/IFrame";
+
+export interface IBackground {
+    sprite: HTMLImageElement;
+    ctx: CanvasRenderingContext2D;
+    frame: IFrame;
+}
--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -1,8 +1,43 @@
+import {Loop} from "./framework26/core/Loop";
+import {settings} from "./settings";
+import {Background} from "./Background";
+
 class Main {
+    private loop: Loop;
+    private sprite: HTMLImageElement;
     private canvas: HTMLCanvasElement;
     private ctx: CanvasRenderingContext2D;
-    private sprite: HTMLImageElement;
+    private background: Background;
 
+    constructor() {
+        this.canvas = document.getElementById(settings.canvasID) as HTMLCanvasElement;
+        this.ctx = this.canvas.getContext('2d');
+
+
+        this.loop = new Loop(() => {
+            this.animate();
+        });
+
+        this.sprite = new Image();
+        this.sprite.src = settings.spriteURL;
+
+        this.background = new Background({
+            ctx: this.ctx,
+            sprite: this.sprite,
+            frame: settings.background.frame
+        });
+
+        this.sprite.addEventListener('load', () => {
+            // ici, on a l'image en mémoire...
+            this.background.draw();
+        });
+
+
+    }
+
+    animate() {
+        // gérer l'animation...
+    }
 }
 
 new Main();
