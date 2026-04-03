94ce30dd8b32e4734afeaca6a718cf9e2beefe12
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2026-04-03 14:03:33 +0200

    add Frame class and integrate into Ground; update frame handling logic

--- src/ts/Ground.ts
+++ src/ts/Ground.ts
@@ -1,5 +1,6 @@
 import {Sprite} from "./framework26/Sprite";
 import {settings} from "./settings";
+import {Frame} from "./framework26/Frame";
 
 export class Ground extends Sprite {
 
@@ -7,13 +8,17 @@ export class Ground extends Sprite {
         super({
             sprite: sprite,
             ctx: ctx,
-            frame: settings.ground.frame,
+            frame: new Frame(settings.ground.frame),
         });
         this.frame.dy = this.ctx.canvas.height - settings.ground.frame.sh;
     }
 
     update() {
         this.frame.dx--;
+
+        if (this.frame.dx <= -(settings.ground.frame.sw - this.ctx.canvas.width)) {
+            this.frame.dx = 0;
+        }
     }
 
     animate() {
---
+++ src/ts/framework26/Frame.ts
@@ -0,0 +1,25 @@
+import {IFrame} from "./interfaces/IFrame";
+
+export class Frame implements IFrame {
+    dx: number;
+    dy: number;
+    dh: number;
+    dw: number;
+    sh: number;
+    sw: number;
+    sx: number;
+    sy: number;
+
+
+    constructor(frame: IFrame) {
+        this.dx = frame.dx;
+        this.dx = frame.dy;
+        this.dw = frame.dw;
+        this.dh = frame.dh;
+
+        this.sx = frame.sx;
+        this.sy = frame.sy;
+        this.sw = frame.sw;
+        this.sh = frame.sh;
+    }
+}
